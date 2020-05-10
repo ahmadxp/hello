402998c1442f58c17f4e43268ba634a80d2ad391
afokay <45879091+afokay@users.noreply.github.com>
2020-05-09 21:54:19 -0400

    Update and rename index.html to hello.py

---
+++ hello.py
@@ -0,0 +1,28 @@
+import os
+
+HEADER = '\033[95m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+BLUE = '\033[94m'
+RESET = '\033[0m'
+
+os.system('clear')
+print (GREEN+"   ***")
+print ("  ** **")
+print (" **   **")
+print (" **   **         **** ")
+print (" **   **       **   ****")
+print (" **  **       *   **   **")
+print ("  **  *      *  **  ***  **")
+print ("   **  *    *  **     **  *")
+print ("    ** **  ** **        **")
+print ("    **   **  **")
+print ("   *           *")
+print ("  *             *")
+print (" *    0     0    *     "+RED+"iChecker")
+print (GREEN+" *   /   @   \   *     "+RED+"-")
+print (GREEN+" *   \__/ \__/   *     "+RED+"iChecker will be make later.")
+print (GREEN+"   *     W     *       "+RED+"author: ./isilent")
+print (GREEN+"     **     **   ")
+print ("       *****")
